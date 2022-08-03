85f12c41730160b66865c2a8654b62fc9e77c70f
ryyip <runyuan.ye@gmail.com>
2022-08-03 16:53:30 +0800

    Update php72 config

--- docker-cp.sh
+++ docker-cp.sh
@@ -1,4 +1,12 @@
+mkdir -p ./docker/data/mysql ./docker/data/redis ./docker/log/proxy/access.log ./docker/log/proxy/error.log ./docker/log/cgi/error.log
+
 docker cp mysql8:/etc/my.cnf ./docker/config/mysql/my.cnf
-docker cp cgi:/usr/local/etc/php/php.ini ./docker/config/cgi/php.ini
-docker cp cgi:/usr/local/etc/php/php.ini-production ./docker/config/cgi/php.ini-production
-docker cp cgi:/usr/local/etc/php/php.ini-development ./docker/config/cgi/php.ini-development
+
+docker cp cgi:/usr/local/etc/php/php.ini ./docker/config/php72/php.ini
+docker cp cgi:/usr/local/etc/php/php.ini-production ./docker/config/php72/php.ini-production
+docker cp cgi:/usr/local/etc/php/php.ini-development ./docker/config/php72/php.ini-development
+docker cp cgi:/usr/local/etc/php/conf.d ./docker/config/php72/conf.d
+
+docker cp cgi:/usr/local/etc/php ./docker/config/php72
+
+kill -USR2 1
--- docker/files/cgi/docker-entrypoint.sh
+++ docker/files/cgi/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# @auther <wangyaxiandev@gmail.com>
+# @auther <runyuan.ye@gmail.com>
 
-/usr/sbin/init
-/usr/sbin/crond
-echo "info: starting php-fpm.."
-/usr/sbin/php-fpm -c /etc/php.ini -y /etc/php-fpm.conf
-tail -f /dev/null
+# /usr/sbin/init
+# /usr/sbin/crond
+# echo "info: starting php-fpm.."
+# /usr/sbin/php-fpm -c /etc/php.ini -y /etc/php-fpm.conf
+# tail -f /dev/null
+
+install-php-extensions sockets
