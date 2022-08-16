16da09da07be8d8798b718dca7b2ba4fea1b42eb
ryyip <runyuan.ye@gmail.com>
2022-08-16 14:42:54 +0800

    opt

--- docker-cp.sh
+++ docker-cp.sh
@@ -1,15 +1,15 @@
 mkdir -p ./docker/data/mysql ./docker/data/redis ./docker/data/rabbitmq 
+mkdir -p ./docker/log/nginx ./docker/log/php72
 
+# 复制docker container的文件夹或文件时, 必须是同名，否则会多创一层文件夹
 docker cp mysql8:/etc/my.cnf ./docker/config/mysql/my.cnf
 docker cp php72:/usr/local/etc/php/php.ini ./docker/config/php72/php.ini
 docker cp php72:/usr/local/etc/php/php.ini-production ./docker/config/php72/php.ini-production
 docker cp php72:/usr/local/etc/php/php.ini-development ./docker/config/php72/php.ini-development
-# 复制docker container文件夹的, 必须是同名，否则会多创一层文件夹
 docker cp php72:/usr/local/etc/php/conf.d ./docker/config/php72/conf.d
 
-docker cp nginx:/var/log/nginx/access.log ./docker/log/nginx/access.log
-docker cp nginx:/var/log/nginx/error.log ./docker/log/nginx/error.log
 docker cp php72:/var/log/php-fpm/error.log ./docker/log/php72/error.log
+docker cp nginx:/var/log/nginx ./docker/log/nginx
 
 # Restart PHP-fpm
 kill -USR2 1
